a53fe3fe1ab22c89f7664a4e74df296c8a5e1e58
Reed Essick <reed.essick@ligo.org>
2016-08-16 18:00:01 -0400

    work towards fake lvalert servers to plug into FakeDb. Not yet completely implemented, but fleshy skeletons are in place

--- lib/ligoTest/lvalert/lvalertTestUtils.py
+++ lib/ligoTest/lvalert/lvalertTestUtils.py
@@ -3,7 +3,62 @@ author      = "reed.essick@ligo.org"
 
 #-------------------------------------------------
 
-'''
-need a tool that monitors a file for updates and extracts lines as they are printed into it.
-this could be tricky due to possible conficts for reading and writing a file, though...
-'''
+import os
+
+import time
+
+#-------------------------------------------------
+
+class LVAlertBuffer():
+    '''
+    a class that wraps around a lvalert.out file produced by FakeDb
+    provides some basic querying and manipulations for scripts that send lvalert messages
+    '''
+
+    def __init__(self, filename):
+        if not os.path.exists(filename):
+            raise ValueError('could not find filename=%s'%filename)
+        self.filename  = filename
+        self.setTimestamp()
+
+    def getTimestamp(self):
+        '''
+        queries the timestamp associated with this file
+        '''
+        return os.path.getmtime(self.filename)
+
+    def setTimestamp(self):
+        '''
+        updates the timestamp for this file
+        '''
+        self.timestamp = self.getTimestamp()
+
+    def wasTouched(self):
+        '''
+        determines whether the file has been modified
+        '''
+        return self.timestamp!=self.getTimestamp()
+
+    def extract(self):
+        '''
+        extracts the new messages and returns them
+
+        NOT IMPLEMENTED
+        '''
+        raise NotImplementedError 
+
+    def monitor(self, foo, cadence=0.1, *args, **kwargs):
+        '''
+        monitors the file, and when a change is detected we extract the call foo with signature:
+        for node, message in self.extract():
+            foo( node, message, *args, **kwargs )
+        '''
+        while True:
+            t = time.time()
+            if self.wasTouched():
+                self.setTimestamp() ### update
+                for node, message in self.extract():
+                    foo( node, message, *args, **kwargs )
+            wait = cadence - (time.time()-t)
+            if wait:
+                time.sleep(wait)
